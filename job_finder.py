fad0a3feeb2382e3c6f837471cf12fd8f164f33e
Dementjev1 <nikita.dementjev.vv@gmail.com>
2026-05-14 16:34:20 +0300

    Sprint 2 progress: scraper can log in and find correct page on linked in, just needs to find correct paragraphs on a complicated job search page.

--- job_finder.py
+++ job_finder.py
@@ -1,3 +1,5 @@
+import logging
+
 import os
 import agentql
 from playwright.sync_api import sync_playwright
@@ -9,27 +11,92 @@ load_dotenv()
 # Professional Practice: Configure the API Key
 agentql.configure(api_key=os.getenv("AGENTQL_API_KEY"))
 
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+
+LOGGING_QUERY = """
+{
+    email_or_username
+    password
+    login_button
+
+}
+"""
+
+JOB_SEARCH_QUERY = """
+{ 
+    search_input
+}
+"""
+
+SEE_OPTIONS_QUERY = """
+{
+    options[] {option_name}
+}
+"""
+
 JOB_QUERY = """
 {
     jobs[] {
-        title
-        company
-        location
-        description_text
+        job_name
     }
 }
 """
 
-URL = r'https://www.cvkeskus.ee/toopakkumised?op=search&search%5Bjob_salary%5D=3&ga_track=results&search%5Blocations%5D%5B%5D=3&search%5Bkeyword%5D=Data+scientist&badge%5Bjob_lang%5D=on&search%5Bexpires_days%5D=&search%5Bjob_lang%5D=en&search%5Bsalary%5D='
+JOB_SCRAP = """
+{
+    job_title
+    company_name
+    location
+    description_text[] {unfold_description_button_link}
+}
+"""
+
+URL = r'https://www.linkedin.com/login/?trk=guest_homepage-basic_nav-header-signin'
+
+job_list = []
 
 with sync_playwright() as p, p.chromium.launch(headless=False) as browser:
     page = agentql.wrap(browser.new_page())
     page.goto(URL)
 
     try:
-        response = page.query_data(JOB_QUERY)
-        for job in response['jobs']:
-            print(job['title'], job['company'], job['location'], job['description_text'])
+        response = page.query_elements(LOGGING_QUERY)
+        response.email_or_username.type("nikita.dementjev.vv@gmail.com", delay=75)
+        response.password.type("Thenbhd2200", delay=75)
+        response.login_button.click()
+
+        page.wait_for_timeout(3000)  # Wait for 3 seconds to ensure the page has loaded after login
+
+        response = page.query_elements(JOB_SEARCH_QUERY)
+        response.search_input.type("Data Scientist", delay=75)
+        
+        response = page.query_elements(SEE_OPTIONS_QUERY)
+        response.options[0].option_name.click()
+
+        page.wait_for_timeout(3000)  # Wait for 3 seconds to ensure the page has loaded after clicking on Jobs
+
+        response = page.query_elements(JOB_QUERY)
+        for jb in response.jobs:
+            jb.job_name.click()
+            page.wait_for_timeout(2000)
+
+            rp = page.query_data(JOB_SCRAP)
+            job_list.append({
+                "job_title": rp['job_title'],
+                "company_name": rp['company_name'],
+                "location": rp['location'],
+                "description_text": rp['description_text']
+            })
+
+            if len(job_list) >= 5:  # Limit to the first 5 job postings for demonstration
+                break
+
+        print(job_list)
+        # response = page.query_data(JOB_QUERY)
+        # for job in response['jobs']:
+        #     print(job['title'], job['company'], job['location'], job['description_text'])
 
     except Exception as e:
         print(f"An error occurred: {e}")
